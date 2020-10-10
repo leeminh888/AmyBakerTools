9a10dd19200bcb2728fa638af5f4d21e507b11d6
issacabraham-git <70165330+issacabraham-git@users.noreply.github.com>
2020-10-10 14:35:18 +0300

    moooooooore

--- roms/10/Flyme/make.sh
+++ roms/10/Flyme/make.sh
@@ -26,3 +26,22 @@ rm -rf $1/app/com.wolfsonmicro.ez2control
 
 # Append file_context
 cat $thispath/file_contexts >> $1/etc/selinux/plat_file_contexts
+
+#to improve the perfomance of GSIs
+echo "ro.sys.fw.dex2oat_thread_count=4" >> $1/build.prop
+echo "dalvik.vm.boot-dex2oat-threads=8" >> $1/build.prop
+echo "dalvik.vm.dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.image-dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.dex2oat-filter=speed" >> $1/build.prop
+echo "dalvik.vm.heapgrowthlimit=256m" >> $1/build.prop
+echo "dalvik.vm.heapstartsize=8m" >> $1/build.prop
+echo "dalvik.vm.heapsize=512m" >> $1/build.prop
+echo "dalvik.vm.heaptargetutilization=0.75" >> $1/build.prop
+echo "dalvik.vm.heapminfree=512k" >> $1/build.prop
+echo "dalvik.vm.heapmaxfree=8m" >> $1/build.prop
+echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+
+
+#Navbar fix
+echo "qemu.hw.mainkeys=0" >> $1/build.prop
+
