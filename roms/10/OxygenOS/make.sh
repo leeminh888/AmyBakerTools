9d4786f11677506aa3043e0596ca53895991d298
issacabraham-git <70165330+issacabraham-git@users.noreply.github.com>
2020-10-10 14:34:24 +0300

    add more

--- roms/10/OxygenOS/make.sh
+++ roms/10/OxygenOS/make.sh
@@ -35,3 +35,34 @@ cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 ## Fix OOS Data, kang from flyme fix
 # Permission
 chmod 0644 $1/etc/init/flymedata.rc
+
+# batteryDrain Fix
+rm -rf $1/bin/logcat
+rm -rf $1/etc/init/hdrfix
+rm -rf $1/bin/hdrfix
+
+#To Boot OnePlus7T and OnePlus7TPro
+cp $thispath/feature_list $1/etc
+chmod 0644 $1/etc/feature_list
+
+#To Boot OnePlus8 series 
+#cp $thispath/surfaceflinger $1/bin
+
+#to improve the perfomance of GSIs
+echo "ro.sys.fw.dex2oat_thread_count=4" >> $1/build.prop
+echo "dalvik.vm.boot-dex2oat-threads=8" >> $1/build.prop
+echo "dalvik.vm.dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.image-dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.dex2oat-filter=speed" >> $1/build.prop
+echo "dalvik.vm.heapgrowthlimit=256m" >> $1/build.prop
+echo "dalvik.vm.heapstartsize=8m" >> $1/build.prop
+echo "dalvik.vm.heapsize=512m" >> $1/build.prop
+echo "dalvik.vm.heaptargetutilization=0.75" >> $1/build.prop
+echo "dalvik.vm.heapminfree=512k" >> $1/build.prop
+echo "dalvik.vm.heapmaxfree=8m" >> $1/build.prop
+echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+
+
+#Navbar fix
+echo "qemu.hw.mainkeys=0" >> $1/build.prop
+
--- roms/10/Pixel/make.sh
+++ roms/10/Pixel/make.sh
@@ -18,3 +18,22 @@ echo "persist.sys.overlay.photness=true" >> $1/etc/prop.default
 echo "qemu.hw.mainkeys=0" >> $1/etc/prop.default
 
 sed -i "/dataservice_app/d" $1/product/etc/selinux/product_seapp_contexts
+
+#to improve the perfomance of GSIs
+echo "ro.sys.fw.dex2oat_thread_count=4" >> $1/build.prop
+echo "dalvik.vm.boot-dex2oat-threads=8" >> $1/build.prop
+echo "dalvik.vm.dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.image-dex2oat-threads=4" >> $1/build.prop
+echo "dalvik.vm.dex2oat-filter=speed" >> $1/build.prop
+echo "dalvik.vm.heapgrowthlimit=256m" >> $1/build.prop
+echo "dalvik.vm.heapstartsize=8m" >> $1/build.prop
+echo "dalvik.vm.heapsize=512m" >> $1/build.prop
+echo "dalvik.vm.heaptargetutilization=0.75" >> $1/build.prop
+echo "dalvik.vm.heapminfree=512k" >> $1/build.prop
+echo "dalvik.vm.heapmaxfree=8m" >> $1/build.prop
+echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+
+
+#Navbar fix
+echo "qemu.hw.mainkeys=0" >> $1/build.prop
+
